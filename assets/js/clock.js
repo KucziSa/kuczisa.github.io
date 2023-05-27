022909e360ff56f641cb6977b9e4e1c91705389d
Hipolit <76755841+KucziSa@users.noreply.github.com>
2023-05-27 22:14:59 +0200

    clock instead of views

--- assets/js/clock.js
+++ assets/js/clock.js
@@ -1,14 +1,17 @@
-function displayClock(){
+function displayClock() {
     var now = new Date();
     var hours = now.getHours();
     var minutes = now.getMinutes();
     var seconds = now.getSeconds();
-
-    var time = hours + " : " + addLeadingZero(minutes) + " : " + addLeadingZero(seconds);
-}
-
-function addLeadingZero(number) {
+  
+    var time = hours + ":" + addLeadingZero(minutes) + ":" + addLeadingZero(seconds);
+  
+    document.getElementById("clock").textContent = time;
+  }
+  
+  function addLeadingZero(number) {
     return (number < 10 ? "0" : "") + number;
-}
-
-setInterval(displayClock, 1000);
+  }
+  
+  // Update the clock every second
+  setInterval(displayClock, 1000);
