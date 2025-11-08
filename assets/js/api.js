4c0af760a7bd8bffebcabbeffc8221989e1961d9
Hipolit <76755841+KucziSa@users.noreply.github.com>
2025-11-08 16:37:48 +0100

    push aa

--- assets/js/api.js
+++
@@ -1,3 +0,0 @@
-fetch("https://api.countapi.xyz/update/kuczisa/views/?amount=1")
-	.then(a=>a.json())
-	.then(b=> document.getElementById('odwiedzenia').innerHTML= "Site has been viewed " + b.value + " times this month.")
